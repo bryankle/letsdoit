9534d2df60317c8f63eb963b53c456ea6667bbf5
Bryan Le <bryan@bryanle.io>
2017-10-15 12:21:06 -0400

    User can be automatically authenticated when revisiting page after signing in

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -6,12 +6,23 @@ import reducers from './reducers';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import reduxThunk from 'redux-thunk'
+import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+// Auto authentication of returning user
+
+// If user has a JWT token, consider the user to be signed in
+const token = localStorage.getItem('token');
+
+if (token) {
+	store.dispatch({ type: AUTH_USER })
+}
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>
     , document.getElementById('root'));
