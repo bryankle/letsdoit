3c86d3609b053a4f8655211b287bfff8ad8af677
Bryan Le <bryan@bryanle.io>
2017-11-09 18:11:45 -0500

    feat: added action to load tasks belonging to group

--- server/client/src/actions/action_group_task.js
+++ server/client/src/actions/action_group_task.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
 import {
-	GROUP_LOAD_TASKS
+	GROUP_LOAD_TASKS,
 	GROUP_ADD_TASK,
 	GROUP_DELETE_TASK,
 	GROUP_COMPLETE_TASK
 } from './types';
 
-export function groupLoadTasks(task) {
+// ('/api/group/:groupId/tasks'
+export function groupLoadTasks(groupId) {
 	return function(dispatch) {
-		axios.
+		console.log('ACTION - groupLoadTasks')
+		axios.get(`/api/group/${groupId}/tasks`)
+			.then(res => {
+				dispatch({
+					type: GROUP_LOAD_TASKS,
+					payload: res.data
+				})
+			})
 	}
 }
