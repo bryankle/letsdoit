c260614d7f31103b65c32950a747918c9680f4ee
Bryan Le <bryan@bryanle.io>
2017-11-09 17:59:06 -0500

    fix: readded fetch message action

--- server/client/src/actions/types.js
+++ server/client/src/actions/types.js
@@ -5,8 +5,9 @@ export const COMPLETE_TASK = 'COMPLETE_TASK';
 export const CLEAR_COMPLETED = 'CLEAR_COMPLETED';
 
 export const AUTH_USER = 'AUTH_USER';
-export const UNAUTH_USER = 'UNAUTH_USER'
+export const UNAUTH_USER = 'UNAUTH_USER';
 export const AUTH_ERROR = 'AUTH_ERROR';
+export const FETCH_MESSAGE = 'FETCH_MESSAGE';
 
 export const LOADING = 'LOADING';
 export const NOT_LOADING = 'NOT_LOADING';
@@ -20,7 +21,7 @@ export const DELETE_GROUP = 'DELETE_GROUP';
 export const EDIT_GROUP = 'EDIT_GROUP';
 export const ADD_TO_GROUP = 'ADD_TO_GROUP';
 
+export const GROUP_LOAD_TASKS =  'GROUP_LOAD_TASKS';
 export const GROUP_ADD_TASK =  'GROUP_ADD_TASK';
 export const GROUP_DELETE_TASK =  'GROUP_DELETE_TASK';
-export const GROUP_LOAD_TASKS =  'GROUP_LOAD_TASKS';
 export const GROUP_COMPLETE_TASK =  'GROUP_COMPLETE_TASK';
