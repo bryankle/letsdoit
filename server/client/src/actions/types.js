a70228c15b230504165dea1be919262396f3cea4
Bryan Le <bryan@bryanle.io>
2017-11-09 17:48:14 -0500

    chore: added group task actions and spaced types by category

--- server/client/src/actions/types.js
+++ server/client/src/actions/types.js
@@ -2,17 +2,25 @@ export const ADD_TASK = 'ADD_TASK';
 export const DELETE_TASK = 'DELETE_TASK';
 export const LOAD_TASKS = 'LOAD_TASKS';
 export const COMPLETE_TASK = 'COMPLETE_TASK';
+export const CLEAR_COMPLETED = 'CLEAR_COMPLETED';
+
 export const AUTH_USER = 'AUTH_USER';
 export const UNAUTH_USER = 'UNAUTH_USER'
 export const AUTH_ERROR = 'AUTH_ERROR';
-export const FETCH_MESSAGE = 'FETCH_MESSAGE';
+
 export const LOADING = 'LOADING';
 export const NOT_LOADING = 'NOT_LOADING';
+
 export const SHOW_SIDEBAR = 'SHOW_SIDEBAR';
 export const HIDE_SIDEBAR = 'HIDE_SIDEBAR';
-export const CLEAR_COMPLETED = 'CLEAR_COMPLETED';
+
 export const LOAD_GROUPS = 'LOAD_GROUPS';
 export const ADD_GROUP = 'ADD_GROUP';
 export const DELETE_GROUP = 'DELETE_GROUP';
 export const EDIT_GROUP = 'EDIT_GROUP';
 export const ADD_TO_GROUP = 'ADD_TO_GROUP';
+
+export const GROUP_ADD_TASK =  'GROUP_ADD_TASK';
+export const GROUP_DELETE_TASK =  'GROUP_DELETE_TASK';
+export const GROUP_LOAD_TASKS =  'GROUP_LOAD_TASKS';
+export const GROUP_COMPLETE_TASK =  'GROUP_COMPLETE_TASK';
