d92aab9e3e3bd279a1d13077c2a4e8b3edb78c5e
Bryan Le <bryan@bryanle.io>
2017-11-09 17:41:26 -0500

    chore: remove comment

--- server/database/models/index.js
+++ server/database/models/index.js
@@ -6,7 +6,7 @@ const Group = require('./group');
 User.hasMany(Task, { as: 'tasks' });
 Group.hasMany(Task, { as: 'tasks' }); 
 Task.belongsTo(Group, { as: 'group' })
-Task.belongsTo(User, { as: 'user' }) // Correct
+Task.belongsTo(User, { as: 'user' })
 
 User.belongsToMany(Group, {through: 'user_group'});
 Group.belongsToMany(User, {through: 'user_group'});
