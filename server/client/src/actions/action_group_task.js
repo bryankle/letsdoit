24daad84bb961ff3583a4225069e8998a52a92f5
Bryan Le <bryan@bryanle.io>
2017-11-09 17:51:42 -0500

    chore: revise order of action

---
+++ server/client/src/actions/action_group_task.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import {
+	GROUP_LOAD_TASKS
+	GROUP_ADD_TASK,
+	GROUP_DELETE_TASK,
+	GROUP_COMPLETE_TASK
+} from './types';
+
+export function groupLoadTasks(task) {
+	return function(dispatch) {
+		axios.
+	}
+}
