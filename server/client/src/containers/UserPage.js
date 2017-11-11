b2913620f6b4048c0b4fbfb23bbd3c0ef9650983
Bryan Le <bryan@bryanle.io>
2017-11-11 17:58:04 -0500

    refactor: prettier

--- server/client/src/containers/UserPage.js
+++ server/client/src/containers/UserPage.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import TodoContainer from './TodoContainer';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import React, { Component } from "react";
+import TodoContainer from "./TodoContainer";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class UserPage extends Component {
-	constructor(props) {
-		super(props)
-	}
-	render() {
-		console.log('UserPage');
-		console.log('localStorage.userId', localStorage.userId)
-		return (
-			<TodoContainer 
-				tasks={this.props.userTasks}
-				completeTask={this.props.completeTask}
-				loading={this.props.loading}
-				loadingComplete={this.props.loadingComplete}
-				addTask={this.props.addTask}
-				id={parseInt(localStorage.userId)}
-			/>
-		)
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    console.log("UserPage");
+    console.log("localStorage.userId", localStorage.userId);
+    return (
+      <TodoContainer
+        tasks={this.props.userTasks}
+        completeTask={this.props.completeTask}
+        loading={this.props.loading}
+        loadingComplete={this.props.loadingComplete}
+        addTask={this.props.addTask}
+        id={parseInt(localStorage.userId)}
+      />
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		userTasks: state.tasks,
-		loading: state.loading
-	}
+  return {
+    userTasks: state.tasks,
+    loading: state.loading
+  };
 }
 
 export default connect(mapStateToProps, actions)(UserPage);
